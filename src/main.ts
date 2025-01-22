192737dcddc4a9a674f8752bce489d3452a031c2
marcusxro <marcussalopaso1@gmail.com>
2025-01-22 21:37:07 +0800

    added helmet protection

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import corsOptions from './users/config/config-options';
 import { ApiKeyGuard } from './Guard/api.key.guard';
 import { ConfigService } from '@nestjs/config';
 import { ApiKeyService } from './Service/api.key.service';
+import helmet from 'helmet';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +16,16 @@ async function bootstrap() {
     credentials: true,
   });
 
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        objectSrc: ["'none'"],
+        upgradeInsecureRequests: [],
+      },
+    }),
+  );
 
 
   await app.listen(process.env.PORT ?? 8080);
